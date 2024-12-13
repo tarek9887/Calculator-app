5438678ab94b63ad20f5a4870d373b634f58259c
tarek9887 <139941002+tarek9887@users.noreply.github.com>
2024-12-13 22:27:44 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// calculate programme...................
+
+const display = document.getElementById("display");
+
+
+function appendToDisplay(input)
+{
+      display.value += input;
+}
+
+function clearDisplay()
+{
+      display.value = "";
+}
+function calculate()
+{
+      try
+      {
+            display.value = eval(display.value);
+      }
+      catch(error)
+      {
+           display.value = "Error";
+      }
+}
+
